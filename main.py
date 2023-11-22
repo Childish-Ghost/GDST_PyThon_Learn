fcff96c54369fea673229715d8f6a9703096f11d
Middl <thestarlake.lukemackin@gmail.com>
2023-11-22 23:54:58 +0800

    首页

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-# print("1: 成绩排序")
+from Grade_sorting import Grade_sorting
+from Grade_Lookup import Grade_Lookup
+
+print("1: 成绩排序", "2: 成绩查找")
 num = int(input("请输入排序数值选择："))
 
 if num == 1:
-    print(execfile("file_B.py"))
+    Grade_sorting()
 else:
-    print("no")
+    if num == 2:
+        Grade_Lookup()
