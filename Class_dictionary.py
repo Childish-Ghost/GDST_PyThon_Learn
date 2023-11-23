e442f61e089bf3b789fe7a547fc306ea4aa3fde0
Middl <thestarlake.lukemackin@gmail.com>
2023-11-23 10:13:59 +0800

    课堂作业_日程表与水果库查询

--- Class_dictionary.py
+++ Class_dictionary.py
@@ -9,16 +9,18 @@ print(fruit)
 
 fruit['苹果'] = '50'
 print("苹果修改", fruit)
-
+print(new_fruit)
 new_fruit.pop('车厘子')
-print("车厘子修改", fruit)
+print("车厘子修改", new_fruit)
 
-fruit_outcome = fruit and new_fruit
+fruit.update(new_fruit)
 
-print(fruit_outcome)
-nn = input("输入你需要查询的水果名称")
+print(fruit)
+print(fruit.values())
 
-for fruit_outcome in nn:
-    print(fruit_outcome[str])
+nn = input("输入你需要查询的水果名称：")
+
+if nn in fruit:
+    print(fruit[nn])
 else:
     print("没有对应存库")
