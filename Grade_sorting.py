b809635a851dfd6f82c2a653f53e1620e6c02b91
Middl <thestarlake.lukemackin@gmail.com>
2023-11-23 13:26:45 +0800

    作业_成绩排序并求平均值

--- Grade_sorting.py
+++ Grade_sorting.py
@@ -1,14 +1,14 @@
 # importation = int(input("输入成绩"))
+def Grade_sorting():
+    raw = [10, 5, 8, 9, 15, 11, 13, 8, 2, 3]
 
-raw = [10, 5, 8, 9, 15, 11, 13, 8, 2, 3]
+    sort = sorted(raw)  # sorted 顺序排序 小→大 ；括号内是数组，元组
 
-sort = sorted(raw)  # sorted 顺序排序 小→大 ；括号内是数组，元组
+    del sort[0], sort[-1]
 
-del sort[0], sort[-1]
+    summation = sum(sort) / len(sort)
 
-summation = sum(sort) / len(sort)
+    print(sort)
 
-print(sort)
-
-print(summation)
-print("2023/11/16")
+    print(summation)
+    print("2023/11/16")
