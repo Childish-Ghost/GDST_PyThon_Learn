49c8801fc2b7983d2d6bde85695897988ace4a7f
Middl <thestarlake.lukemackin@gmail.com>
2023-11-30 10:59:25 +0800

    成绩划分

---
+++ chap4_1.py
@@ -0,0 +1,11 @@
+cj = eval(input("请输入成绩："))
+if cj >= 90:
+    print("成绩等级为A")
+elif 80 <= cj < 90:
+    print("成绩等级为B")
+elif 70 <= cj < 80:
+    print("成绩等级为C")
+elif 60 <= cj < 70:
+    print("成绩等级为D")
+elif cj < 60:
+    print("成绩等级为E")
