6d93da27301d45c782f8253cc22975ccead180e8
Middl <thestarlake.lukemackin@gmail.com>
2023-11-30 11:22:35 +0800

    登录判断

--- PT4_1.py
+++ PT4_1.py
@@ -1,14 +1,14 @@
-date={'5102365492':'123456789'}
+date = {'5102365492': '123456789', '562248522': '2256524561'}
 
-user=input("请输入你的QQ账号：")
+user = input("请输入你的QQ账号：")
 
-password=input("请输入你的QQ密码：")
+password = input("请输入你的QQ密码：")
 
 if user in date:
     name = date[user]
     if password == name:
-        print("1")
+        print("欢迎使用")
     else:
-        print("2")
+        print("密码错误")
 else:
     print("密码错误")
