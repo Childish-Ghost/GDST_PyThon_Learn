22637a60cacc76e766e390f37b2a9ac4db440f92
Student <m13431624369@163.com>
2023-12-14 10:53:46 +0800

    16周作业2

---
+++ 10000 or 4.py
@@ -0,0 +1,10 @@
+ck = float(input("你的存款数量："))
+cs = ck * 2
+y = 1
+ll = float(input("利率："))
+while ck <= cs:
+    ck = ck * ll + ck
+    y += 1
+    print("第", y, "年，本息", '%.2f' % ck)
+    if ck > cs:
+        print("第%d年翻倍" % y)
