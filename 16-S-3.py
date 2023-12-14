263f9b47e5a39b7d343853cc3ec2e11e32d300ee
Student <m13431624369@163.com>
2023-12-14 11:21:32 +0800

    16周作业3

---
+++ 16-S-3.py
@@ -0,0 +1,10 @@
+lower = int(input("输入区间最小值: "))
+upper = int(input("输入区间最大值: "))
+
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, num):  # 创建i数组 数组开始为2，结束为最大值
+            if (num % i) == 0:  # 用原始数组除以i数组内的所有数
+                break
+        else:
+            print(num)
