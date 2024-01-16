2e8fd03a2071dfac7f6e06c6c16ac584ad2d4fc9
MiddlingYak <MiddlingYak5869@outlook.com>
2024-01-16 16:47:00 +0800

    任务4.2 实现列表数据的连加或连乘

---
+++ 16/Class16-1.py
@@ -0,0 +1,15 @@
+#任务4.2 实现列表数据的连加或连乘
+lista = [15, 46, 15, 88, 999, 50]
+
+addition = 0
+multiplication = 1
+
+for i in list:
+    addition = addition + i
+
+for i in list:
+    multiplication = multiplication * i
+# 连加
+print(addition)
+# 连乘
+print(multiplication)
