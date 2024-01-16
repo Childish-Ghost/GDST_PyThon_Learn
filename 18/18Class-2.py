18549a3110b86a7e869a5c24e810009288bedb9a
MiddlingYak <MiddlingYak5869@outlook.com>
2024-01-16 16:50:06 +0800

    #猜数字游戏：随机产生一个1~100之间的数，每次提示猜数范围，直到猜中就公布该数。

--- 18/18Class-2.py
+++ 18/18Class-2.py
@@ -1,9 +1,10 @@
+#猜数字游戏：随机产生一个1~100之间的数，每次提示猜数范围，直到猜中就公布该数。
 import random as r
 
 a = r.randrange(1, 100)
 max = 100
 min = 1
-#print(a)  #//临时查看
+# print(a)  #//临时查看
 while True:
     x = int(input("x:"))
     if 0 < x < 100:
